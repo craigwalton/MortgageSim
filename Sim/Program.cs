65e6d82618528d1723bb5445a5adc935fbc945e1
Craig Walton <craig.w@me.com>
2023-10-18 21:21:49 +0100

    Introduce Mortgage.

--- Sim/Program.cs
+++ Sim/Program.cs
@@ -7,33 +7,22 @@ internal static class Sim
         var initialPropertyValue = new decimal(200_000);
         var deposit = new decimal(50_000);
         var interestRate = InterestRate.FromYearlyPercentage(2.09);
-        const int mortgageTermYears = 25;
-        var initialLoanValue = initialPropertyValue - deposit;
-        var monthlyPayment = ComputeMonthlyPayment();
+        var mortgage = new Mortgage(initialPropertyValue - deposit, interestRate, 25);
 
         var balance = initialPropertyValue - deposit;
 
-        for (var y = 0; y < mortgageTermYears; y++)
+        for (var y = 0; y < mortgage.TermYears; y++)
         {
             for (var m = 0; m < 12; m++)
             {
-                var interest = balance * interestRate.MonthlyDecimal;
-                var principal = monthlyPayment - interest;
-                balance -= principal;
+                var payment = mortgage.ComputePayment(balance);
+                balance -= payment.Principal;
                 Console.WriteLine(
-                    $"M{m:00}/Y{y:00} payment: {monthlyPayment} (interest={interest}; principal={principal})");
+                    $"M{m:00}/Y{y:00} payment: {mortgage.MonthlyPayment} " +
+                    $"(interest={payment.Interest}; principal={payment.Principal})");
             }
         }
 
         Console.WriteLine($"Final mortgage balance: {balance}");
-
-        decimal ComputeMonthlyPayment()
-        {
-            var numberOfPayments = mortgageTermYears * 12;
-            var rate = interestRate.Monthly;
-            var rateToPowerOfPayments = Math.Pow(1 + rate, numberOfPayments);
-            var result = (double)initialLoanValue * (rate * rateToPowerOfPayments) / (rateToPowerOfPayments - 1);
-            return new decimal(Math.Round(result, 2));
-        }
     }
 }
