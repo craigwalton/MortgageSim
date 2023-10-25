f86e94ffe9261f903d153a77b676caeb0f16ce3a
Craig Walton <craig.w@me.com>
2023-10-25 19:01:28 +0100

    Add 3D sensitivity analysis.

--- Sim/Program.cs
+++ Sim/Program.cs
@@ -6,7 +6,7 @@ internal static class Sim
 {
     public static void Main()
     {
-        Run2DSensitivityAnalysis();
+        Run3DSensitivityAnalysis();
     }
 
     private static void RunSensitivityAnalysis()
@@ -42,6 +42,30 @@ internal static class Sim
                     propertyValue: new PropertyValue(200_000, Distributions.Constant(p)),
                     simulationYears: 5);
                 sw.WriteLine($"{i},{p},{result.ComputeDelta():F2}");
+            }
+        }
+    }
+
+    private static void Run3DSensitivityAnalysis()
+    {
+        using var sw = new StreamWriter("../../../../data/3d.csv");
+        sw.WriteLine("interestRate,propertyValueIncrease,rentIncrease,delta");
+        for (var i = -0.05m; i <= 0.2m; i += 0.01m)
+        {
+            for (var p = -0.1m; p <= 0.1m; p += 0.01m)
+            {
+                for (var r = 0.0m; r <= 0.1m; r += 0.01m)
+                {
+                    var mortgageInterestRate = new InterestRate(Distributions.Constant(i + 0.02m));
+                    var savingsInterestRate = new InterestRate(Distributions.Constant(i));
+                    var result = new Simulation().Run(
+                        mortgageInterestRate: mortgageInterestRate,
+                        savingsInterestRate: savingsInterestRate,
+                        rent: new RentPrice(700m, Distributions.Constant(r)),
+                        propertyValue: new PropertyValue(200_000, Distributions.Constant(p)),
+                        simulationYears: 5);
+                    sw.WriteLine($"{i},{p},{r},{result.ComputeDelta():F2}");
+                }
             }
         }
     }
