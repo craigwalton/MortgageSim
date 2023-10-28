8e1d41c3497c58f02fe176392e15135e727d2f96
Craig Walton <craig.w@me.com>
2023-10-28 09:25:28 +0100

    Add RunSim() to compare results of averaging many stochastic simulations versus one at the mean.

--- Sim/Program.cs
+++ Sim/Program.cs
@@ -6,7 +6,8 @@ internal static class Sim
 {
     public static void Main()
     {
-        Run3DSensitivityAnalysis();
+        RunStochasticSim();
+        RunSim();
     }
 
     private static void RunSensitivityAnalysis()
@@ -28,20 +29,20 @@ internal static class Sim
     private static void Run2DSensitivityAnalysis()
     {
         using var sw = new StreamWriter("../../../../data/2d.csv");
-        sw.WriteLine("interestRate,propertyValueIncrease,delta");
+        sw.WriteLine("interestRate,initialRent,delta");
         for (var i = -0.05m; i <= 0.2m; i += 0.01m)
         {
-            for (var p = -0.1m; p <= 0.1m; p += 0.01m)
+            for (var r = 500m; r <= 2000m; r += 100m)
             {
                 var mortgageInterestRate = new InterestRate(Distributions.Constant(i + 0.02m));
                 var savingsInterestRate = new InterestRate(Distributions.Constant(i));
                 var result = new Simulation().Run(
                     mortgageInterestRate: mortgageInterestRate,
                     savingsInterestRate: savingsInterestRate,
-                    rent: new RentPrice(700m, Distributions.Constant(0.03)),
-                    propertyValue: new PropertyValue(200_000, Distributions.Constant(p)),
+                    rent: new RentPrice(r, Distributions.Constant(0.03)),
+                    propertyValue: new PropertyValue(200_000, Distributions.Constant(0.03)),
                     simulationYears: 5);
-                sw.WriteLine($"{i},{p},{result.ComputeDelta():F2}");
+                sw.WriteLine($"{i},{r},{result.ComputeDelta():F2}");
             }
         }
     }
@@ -72,12 +73,31 @@ internal static class Sim
 
     private static void RunStochasticSim()
     {
-        const int count = 2000;
+        const int count = 50000;
         var results = new List<Simulation.Result>(count);
         for (var i = 0; i < count; i++)
         {
             var simulation = new Simulation();
             var result = simulation.Run();
+            results.Add(result);
+        }
+        var purchaseAverageEquity = results.Average(x => x.PurchaseEquity);
+        var rentAverageEquity = results.Average(x => x.RentEquity);
+        Console.WriteLine($"Avg Purchase equity={purchaseAverageEquity:C}; Avg Rent equity={rentAverageEquity:C}");
+    }
+
+    private static void RunSim()
+    {
+        const int count = 1;
+        var results = new List<Simulation.Result>(count);
+        for (var i = 0; i < count; i++)
+        {
+            var simulation = new Simulation();
+            var result = simulation.Run(
+                mortgageInterestRate: new InterestRate(Distributions.Constant(0.0209)),
+                savingsInterestRate: new InterestRate(Distributions.Constant(0.001)),
+                rent: new RentPrice(500m, Distributions.Constant(0.02)),
+                propertyValue: new PropertyValue(200_000m, Distributions.Constant(0.03)));
             results.Add(result);
         }
         var purchaseAverageEquity = results.Average(x => x.PurchaseEquity);
