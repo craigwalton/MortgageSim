413933fbb95b9169c4237e886ca3b730bbc012b5
Craig Walton <craig.w@me.com>
2023-10-29 10:18:52 +0000

    Update baseline figures to reflect best estimates.

--- Sim/Baseline.cs
+++ Sim/Baseline.cs
@@ -8,8 +8,8 @@ public static class Baseline
     public const decimal Deposit = 30_000m;
     public const int MortgageTermYears = 25;
 
-    public static readonly PropertyValue PropertyValue = new(200_000m, 0.03m);
-    public static readonly InterestRate MortgageInterestRate = new(0.02m);
-    public static readonly RentPrice RentPrice = new(500m, 0.02m);
-    public static readonly InterestRate SavingsInterestRate = new(0.02m);
+    public static readonly PropertyValue PropertyValue = new(200_000m, 0.025m);
+    public static readonly InterestRate MortgageInterestRate = new(0.05m);
+    public static readonly RentPrice RentPrice = new(1200m, 0.03m);
+    public static readonly InterestRate SavingsInterestRate = new(0.05m);
 }
