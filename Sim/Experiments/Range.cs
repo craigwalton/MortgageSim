a3bba2fcc206f798612f3d725776625acce1a9d0
Craig Walton <craig.w@me.com>
2023-10-30 20:59:27 +0000

    Make Range a class rather than struct as its instance size is >16 bytes.

--- Sim/Experiments/Range.cs
+++ Sim/Experiments/Range.cs
@@ -1,6 +1,6 @@
 namespace PropertySim.Experiments;
 
-public readonly record struct Range(decimal Start, decimal Stop, decimal Step)
+public sealed record Range(decimal Start, decimal Stop, decimal Step)
 {
     public IEnumerable<decimal> Enumerate()
     {
