a372374a74bfa039f571b1e7d3af9782159604a7
Craig Walton <craig.w@me.com>
2023-11-02 18:29:21 +0000

    Add Savings TODO.

--- Sim/Accounts/Savings.cs
+++ Sim/Accounts/Savings.cs
@@ -18,8 +18,14 @@ public class Savings
 
     public void MakeMonthlyPayment(decimal payment)
     {
+        // TODO: Should we allow negative payments? Perhaps only if it doesn't result in the balance being negative?
         var accruedInterest = Balance * _interestRate.Monthly;
         Balance += accruedInterest + payment;
+        // if (Balance < 0m)
+        // {
+        //     throw new InvalidOperationException(
+        //         $"A payment of {payment:C} will result in this account being overdrawn: {Balance:C}.");
+        // }
         _output.WriteLine($"Savings payment={payment:C}; Interest={accruedInterest:C}; Balance={Balance:C}");
     }
 }
