c21441771c60370798231721f6dfc200a68744ab
Craig Walton <craig.w@me.com>
2023-11-04 18:01:55 +0000

    Drop "years" from column names in favour of term/duration.

--- Sim/Csv/Columns.cs
+++ Sim/Csv/Columns.cs
@@ -2,11 +2,11 @@ namespace PropertySim.Csv;
 
 public static class Columns
 {
-    public const string SimulationYears = "simulationYears";
+    public const string SimulationYears = "simulationDuration";
     public const string Deposit = "deposit";
     public const string InitialPropertyValue = "initialPropertyValue";
     public const string PropertyValueYearlyIncrease = "propertyValueYearlyIncrease";
-    public const string MortgageTermYears = "mortgageTermYears";
+    public const string MortgageTermYears = "mortgageTerm";
     public const string MortgageInterestRate = "mortgageInterestRate";
     public const string InitialMonthlyRentPrice = "initialMonthlyRentPrice";
     public const string RentPriceYearlyIncrease = "rentPriceYearlyIncrease";
