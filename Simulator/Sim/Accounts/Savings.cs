f4bb8912192ac642c32eb4d19e967e3c7e181966
Craig Walton <craig.w@me.com>
2023-11-06 21:32:41 +0000

    Add comment explaining rationale for allowing overdraft.

--- Simulator/Sim/Accounts/Savings.cs
+++ Simulator/Sim/Accounts/Savings.cs
@@ -17,14 +17,13 @@ public sealed class Savings
 
     public void MakeMonthlyPayment(decimal payment)
     {
-        // TODO: Should we allow negative payments? Perhaps only if it doesn't result in the balance being negative?
+        // Note: negative payments (withdrawals) are permitted, even if the account becomes overdrawn.
+        // Typically, a savings account would forbid overdrafts or would charge a higher interest rate on the overdraft.
+        // However, given that the goal is to simply compute the difference in net worth between buying and renting
+        // without any consideration of affordability, we assume that any additional funds required in the rent
+        // scenario (the overdraft) would have been deposited in an equivalent savings account in the mortgage scenario.
         var accruedInterest = Balance * _interestRate.Monthly;
         Balance += accruedInterest + payment;
-        // if (Balance < 0m)
-        // {
-        //     throw new InvalidOperationException(
-        //         $"A payment of {payment:C} will result in this account being overdrawn: {Balance:C}.");
-        // }
         Debug.WriteLine($"Savings payment={payment:C}; Interest={accruedInterest:C}; Balance={Balance:C}");
     }
 }
