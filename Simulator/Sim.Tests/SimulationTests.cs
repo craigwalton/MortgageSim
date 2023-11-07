d4e32afb513ef14d1efd1d4fdec187b251b110e6
Craig Walton <craig.w@me.com>
2023-11-07 21:21:46 +0000

    Improve Simulation test coverage.

--- Simulator/Sim.Tests/SimulationTests.cs
+++ Simulator/Sim.Tests/SimulationTests.cs
@@ -25,6 +25,32 @@ public sealed class SimulationTests
     }
 
     [Fact]
+    public void Verify_ground_truth_2()
+    {
+        var sut = new Simulation(
+            simulationYears: 5,
+            deposit: 30_000m,
+            propertyValue: new PropertyValue(200_000, 0.03m),
+            mortgageInterestRate: new InterestRate(0.06m),
+            rent: new RentPrice(1000m, 0.03m),
+            savingsInterestRate: new InterestRate(0.10m));
+
+        var actual = sut.Run();
+
+        Assert.Equal(78_970.27m, actual.PurchaseEquity, precision: 2);
+        Assert.Equal(52_433.60m, actual.RentEquity, precision: 2);
+        Assert.Equal(26_536.67m, actual.ComputeDelta(), precision: 2);
+    }
+
+    [Fact]
+    public void Can_run_simulation_until_mortgage_paid()
+    {
+        var sut = new Simulation(simulationYears: 25);
+
+        sut.Run();
+    }
+
+    [Fact]
     public void Handles_zero_interest()
     {
         var sut = new Simulation(
